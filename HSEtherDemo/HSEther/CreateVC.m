dcc0d47cf7aea7f73e712a49660a9b9933611fec
colinhu <hcw1314520@163.com>
2018-07-25 12:58:46 +0800

    update some method

--- HSEtherDemo/HSEther/CreateVC.m
+++ HSEtherDemo/HSEther/CreateVC.m
@@ -23,21 +23,9 @@
     
     //创建钱包 等5秒钟，创建比较慢
     [HSEther hs_createWithPwd:@"11111111" block:^(NSString *address, NSString *keyStore, NSString *mnemonicPhrase, NSString *privateKey) {
-        NSLog(@"\n\n%@\n%@\n%@\n%@\n\n提供 非静态库源码，包括iOS和Android\n提供 技术支持，包括iOS和Android\n联系wechat： iHoushuai",address,keyStore,mnemonicPhrase,privateKey);
-        NSLog(@"我建了一个微信ios以太坊开发收费群（每人200元），里面有此静态库源文件，我还在群里做技术解答。若感兴趣可以加我微信iHoushuai");
-        
         self.centerLabel.text = [NSString stringWithFormat:@"【钱包地址address】%@\n\n【官方keyStore】%@\n\n【助记词mnemonicPhrase】%@\n\n【私钥privateKey】%@\n",address,keyStore,mnemonicPhrase,privateKey];
     }];
-    
-    
-    //更多详情见HSEther
-    //欢迎点星星 https://github.com/wolfhous/HSEther
-    
-    
-    //
-    // 提供 非静态库源码，包括iOS和Android
-    // 提供 技术支持，包括iOS和Android
-    // 联系wechat： iHoushuai
+
 }
 
 
--- HSEtherDemo/HSEther/ViewController.m
+++ HSEtherDemo/HSEther/ViewController.m
@@ -21,10 +21,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //2018-06-04
-    //最近有很多人加我微信，全是问钱包 创建 导入 签名 验签 查询 转账 web3配置 ....
-    //我建了一个微信ios以太坊开发收费群（每人200元），里面有此静态库源文件，我还在群里做技术解答。若感兴趣可以加我微信iHoushuai
-    
     self.title = @"HSEther.a 1.2";
     
 }
