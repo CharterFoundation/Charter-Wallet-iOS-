bd413ab6071f7c8bcdb867aa0b8c7225721c405b
colinhu <hcw1314520@163.com>
2018-07-25 13:01:14 +0800

    update some method

--- HSEtherDemo/HSEther/BalanceVC.m
+++ HSEtherDemo/HSEther/BalanceVC.m
@@ -18,23 +18,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
+
     self.title = @"hs_getBalanceWithTokens";
 
-    
     [HSEther hs_getBalanceWithTokens:@[@"0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0",@"0x0806552bc66a44d9ebe4af007cbb93114c4c0a76"] withAddress:@"0xe83aec696478bba2404eaa0eb9b1f2d58269d637" block:^(NSArray *arrayBanlance, BOOL suc) {
-        
-        
+
         NSLog(@"\n\n\n以太坊地址【0xe83aec696478bba2404eaa0eb9b1f2d58269d637】的\n以太坊余额为%@\nEOS余额为%@\nZAG余额为%@\n关于余额为何特别长，因为不同代币数量小数位数是不一样的，比如以太坊可以保留18位小数\n\n\n提供 非静态库源码，包括iOS和Android\n提供 技术支持，包括iOS和Android\n联系wechat： iHoushuai",arrayBanlance.firstObject,arrayBanlance[1],arrayBanlance.lastObject);
 
-        
         self.centerLabel.text = [NSString stringWithFormat:@"以太坊地址【0xe83aec696478bba2404eaa0eb9b1f2d58269d637】的\n\n以太坊余额为 %@\n\nEOS余额为 %@\n\nZAG余额为 %@\n\n\n关于余额为何特别长，因为不同代币数量小数位数是不一样的，比如以太坊可以保留18位小数",arrayBanlance.firstObject,arrayBanlance[1],arrayBanlance.lastObject];
         
         //余额验证请点击
         //https://etherscan.io/address/0xe83aec696478bba2404eaa0eb9b1f2d58269d637
     }];
 }
-
 
 @end
